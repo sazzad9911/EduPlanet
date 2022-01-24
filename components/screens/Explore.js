b4febd0402925e7af283fe332f03de25e0ca80f0
Md.Jannatul Ferdous (Emon) <91200477+jfmemon@users.noreply.github.com>
2022-01-25 04:02:09 +0600

    Explore 40% done.

--- components/screens/Explore.js
+++ components/screens/Explore.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import style from '../styles/style'
 const Explore = () => {
     return (
-        <View>
-            <Text style={style.headLine}>Explore</Text>
+        <View style={style.allOver}>
+            <View style={style.textView}>
+                <Text style={style.headLine}>Explore Online{"\n"}Courses{"\n"}</Text>
+                <Text style={style.text1}>All types of educational & {"\n"}professional courses are{"\n"}available in online.</Text>
+                <View>
+                    <Image
+                        style={style.image}
+                        source={{
+                            uri: 'https://ysseglobal.org/blog/wp-content/uploads/2019/09/YSSE-BLOG-2003-1.jpg'
+                        }}
+                    />
+                </View>
+            </View>
         </View>
     );
 };
--- components/styles/style.js
+++ components/styles/style.js
@@ -1,26 +1,74 @@
-import { StyleSheet, Dimensions} from "react-native";
-const window=Dimensions.get("window")
+import { StyleSheet, Dimensions } from "react-native";
+const window = Dimensions.get("window")
 
 const style = StyleSheet.create({
-    headLine:{
-        fontSize:25,
-        fontWeight:'700',
-        textAlign:'center',
-        color:'black'
+    headLine: {
+        fontSize: 30,
+        fontWeight: '800',
+        color: 'black'
     },
-    text:{
-        fontSize:16,
-        fontWeight:'200',
-        color:'green',
+    text: {
+        fontSize: 16,
+        fontWeight: '200',
+        color: 'green',
     },
-    tabButton:{
-        width:100,
-        height:40,
-        backgroundColor:'white',
-        margin:10,
-        justifyContent:'center',
+    tabButton: {
+        width: 100,
+        height: 40,
+        backgroundColor: 'white',
+        margin: 10,
+        justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
+
+    allOver: {
+        height: 792,
+        width: 432,
+        backgroundColor: 'white',
+        borderTopLeftRadius: 40,
+        borderTopRightRadius: 40,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 1,
+    },
+
+    textView: {
+        height: 300,
+        width: 220,
+        backgroundColor: 'white',
+        marginTop: 100,
+        marginLeft: 45,
+    },
+
+    text1: {
+        fontSize: 20,
+        color: 'black',
+        fontWeight: '600',
+        width: 400
+    },
+
+    image: {
+        height: 200,
+        width: 350,
+        backgroundColor: 'red',
+        marginTop: 50,
+        borderRadius: 12
+    },
+
+    bottomView: {
+        height: 10,
+        width: 10,
+        borderRadius: 100,
+        backgroundColor: 'black'
+    },
+
+    dotView: {
+        height: 70,
+        width: 150,
+        backgroundColor: 'red',
+        marginTop: 100
+    },
 })
 
 export default style
