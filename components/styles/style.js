48cbee39d6fc428696393441a60ad5e5893cb9f3
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2022-01-24 21:54:19 +0000

    button done

--- components/styles/style.js
+++ components/styles/style.js
@@ -6,7 +6,9 @@ const style = StyleSheet.create({
         fontSize:25,
         fontWeight:'700',
         textAlign:'center',
-        color:'black'
+        color:'black',
+        marginBottom:35,
+        marginTop:30
     },
     text:{
         fontSize:16,
@@ -20,7 +22,32 @@ const style = StyleSheet.create({
         margin:10,
         justifyContent:'center',
         alignItems: 'center',
-    }
+    },
+    viewsignup:{
+        
+        borderRadius: 40,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    signUpButton:{
+        height: 40,
+        width:300,
+        
+        borderWidth: 0.5,
+        padding: 10,
+        borderRadius:20,
+        backgroundColor:'#dda0dd'
+    },
+    input: {
+        height: 40,
+        width:300,
+        marginTop:30,
+        borderWidth: 0.5,
+        padding: 10,
+        borderRadius:20,
+        backgroundColor:'#ffe4e1'
+      },
+
 })
 
 export default style
