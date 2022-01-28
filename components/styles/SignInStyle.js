ad6ea60a8f7afa2d98a1eee8404a4c1bf06a3d36
Md.Jannatul Ferdous (Emon) <91200477+jfmemon@users.noreply.github.com>
2022-01-28 22:28:59 +0600

    hello

---
+++ components/styles/SignInStyle.js
@@ -0,0 +1,51 @@
+import { StyleSheet, Dimensions, Text } from "react-native";
+const window = Dimensions.get("window")
+
+const SignInStyle = StyleSheet.create({
+    textInput: {
+        height: window.height - 650,
+        width: window.width,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    underLogin: {
+        height: window.height - 750,
+        width: window.width,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+
+    underDotview: {
+        height: 17,
+        width: 17,
+        borderRadius: 4,
+        backgroundColor: 'black',
+        marginLeft: -320,
+        marginTop: 5
+    },
+
+    underDotview1: {
+        height: 13,
+        width: 13,
+        borderRadius: 4,
+        backgroundColor: 'white',
+        marginLeft: 1,
+        marginTop: 1
+    },
+
+    bottomSignIn: {
+        height: window.height - 550,
+        width: window.width,
+        backgroundColor: '#b0c4de',
+        borderTopLeftRadius: 400,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+})
+
+export default SignInStyle
